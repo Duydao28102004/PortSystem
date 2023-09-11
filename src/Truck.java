e12d8302ac5e33af9b428cdc9d5cb269aaf7abe4
Andy Nguyen <ndnguyen.ad@gmail.com>
2023-09-12 01:59:37 +0700

    Add Truck class

--- src/Truck.java
+++ src/Truck.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ship extends Vehicle {
-    private List<Trip> trips; // Keep track of trips made by the ship.
+public class Truck extends Vehicle {
+    private List<Trip> trips; // Keep track of trips made by the truck.
 
-    public Ship(String id, String name, double currentFuel, double carryingCapacity, double fuelCapacity) {
+    public Truck(String id, String name, double currentFuel, double carryingCapacity, double fuelCapacity) {
         super(id, name, currentFuel, carryingCapacity, fuelCapacity);
         this.trips = new ArrayList<>();
     }
 
     @Override
     public boolean canMoveToPort(Port destinationPort) {
-        // A ship can move to a port if the destination port has landing ability.
+        // A truck can move to a port if the destination port has landing ability.
         return destinationPort.isLandingAbility();
     }
 
     @Override
     public void moveToPort(Port destinationPort) {
-        // Check if the ship can move to the destination port.
+        // Check if the truck can move to the destination port.
         if (!canMoveToPort(destinationPort)) {
             System.out.println("Cannot move to the destination port.");
             return;
@@ -58,18 +58,18 @@ public class Ship extends Vehicle {
     public void refuel() {
         // Refill the fuel tank to its capacity.
         this.currentFuel = this.fuelCapacity;
-        System.out.println("Ship refueled to capacity.");
+        System.out.println("Truck refueled to capacity.");
     }
 
     @Override
     public void loadContainer(Container container) {
-        // Check if the ship has enough capacity and the container type is allowed.
+        // Check if the truck has enough capacity and the container type is allowed.
         if (totalContainers + 1 <= carryingCapacity && isContainerTypeAllowed(container.getType())) {
             containers.add(container);
             totalContainers++;
-            System.out.println("Loaded container onto the ship.");
+            System.out.println("Loaded container onto the truck.");
         } else {
-            System.out.println("Cannot load the container onto the ship.");
+            System.out.println("Cannot load the container onto the truck.");
         }
     }
 
@@ -78,19 +78,30 @@ public class Ship extends Vehicle {
         if (containers.contains(container)) {
             containers.remove(container);
             totalContainers--;
-            System.out.println("Unloaded container from the ship.");
+            System.out.println("Unloaded container from the truck.");
         } else {
-            System.out.println("Container not found on the ship.");
+            System.out.println("Container not found on the truck.");
         }
     }
 
-    // Check if a container type is allowed on the ship.
-    private boolean isContainerTypeAllowed(Container container) {
-        return true;
+    // Check if a container type is allowed on the truck.
+    public boolean isContainerTypeAllowed(Container container) {
+        Container type = container.getType();
+        switch (type) {
+            case DRY_STORAGE:
+            case OPEN_TOP:
+            case OPEN_SIDE:
+                return true; // Basic trucks can carry dry storage, open top, and open side containers.
+            default:
+                return false;
+        }
     }
 
-    // Additional method to get the list of trips made by the ship.
+    // Additional method to get the list of trips made by the truck.
     public List<Trip> getTrips() {
         return trips;
     }
 }
+
+    
+
