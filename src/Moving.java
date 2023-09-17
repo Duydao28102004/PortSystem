d02bf95f93862ac477d3d5b6f947cab1519b6732
Duydao28102004 <119027061+Duydao28102004@users.noreply.github.com>
2023-09-17 18:52:45 +0700

    continue build moving function

--- src/Moving.java
+++ src/Moving.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -52,29 +53,149 @@ public class Moving {
         }
 
         Vehicle selectedVehicle = vehiclesInStartingPort.get(selectedVehicleID - 1);
-
-        // Get a list of containers in the starting port
-        List<Container> containersInStartingPort = startingPort.getContainers();
-
-        if (containersInStartingPort.isEmpty()) {
-            System.out.println("No containers found in port " + startingPort.getPortName());
+        // Check if destination port available
+        if (!isDestinationPortAvailable(destinationPort, selectedVehicle)) {
+            System.out.println("The destination port do not have enough space");
             return;
         }
-        // Now you can use 'containersInStartingPort' for further processing
+        // Get a list of containers in the starting port suitable for the type of vehicle
+        List<Container> compatibleContainersInStartingPort = new ArrayList<>();
+
+        for (Container container : startingPort.getContainers()) {
+            if (isContainerCompatibleWithVehicle(container, selectedVehicle)) {
+                compatibleContainersInStartingPort.add(container);
+            }
+        }
+
+        if (compatibleContainersInStartingPort.isEmpty()) {
+            System.out.println("There are no suitable containers for the selected vehicle.");
+            return;
+        }
 
         // Create a list to hold the selected containers
         List<Container> selectedContainers = new ArrayList<>();
-        int userchoice = 1;
-        while (userchoice == 1) {
-            containerCRUD.printContainersInPort(containersInStartingPort);
-            System.out.print("Enter container ID you want to load on vehicle");
+        double totalWeight = 0;
+        double remainingWeight = selectedVehicle.getCarryingCapacity();
+        while (true) {
+            containerCRUD.printContainersInPort(compatibleContainersInStartingPort);
+            System.out.print("Enter container ID you want to load on the vehicle (0 to exit): ");
             int selectedContainerID = scanner.nextInt();
-            selectedContainers.add(containersInStartingPort.get(selectedContainerID - 1));
-            containersInStartingPort.remove(selectedContainerID-1);
-            System.out.print("Do you want to add more containers?(1 for yes, 0 for no):");
-            userchoice = scanner.nextInt();
+
+            if (selectedContainerID == 0) {
+                break; // Exit the loop if the user enters 0
+            }
+
+            // Find the selected container by ID and move it to the selectedContainers list
+            Container selectedContainer = null;
+            Iterator<Container> iterator = compatibleContainersInStartingPort.iterator();
+            while (iterator.hasNext()) {
+                Container container = iterator.next();
+                if (container.getC_number() == selectedContainerID) {
+                    selectedContainer = container;
+                    if (selectedContainer.getWeight() <= remainingWeight) {
+                        iterator.remove(); // Remove the selected container from containersInStartingPort
+                        selectedContainers.add(selectedContainer); // Add it to the selectedContainers list
+                        totalWeight = totalWeight + selectedContainer.getWeight();// Update the total weight
+                        remainingWeight = remainingWeight - selectedContainer.getWeight();
+                        System.out.println("Current Total Weight: " + totalWeight);
+                        System.out.println("Remaining Weight Capacity: " + (selectedVehicle.getCarryingCapacity() - totalWeight));
+                        break;
+                    }
+                    System.out.println("The container is overweight");
+                }
+            }
+            if (selectedContainer == null) {
+                System.out.println("Container not found");
+            }
+        }
+        double totalFuelConsumption = totalFuelConsumptionForTrip(selectedContainers,startingPort,destinationPort,selectedVehicle);
+        System.out.println(selectedContainers.size());
+        System.out.println(totalFuelConsumption);
+        // Now you have a list of compatible containers in 'selectedContainers'
+    }
+
+
+    static boolean isContainerCompatibleWithVehicle(Container container, Vehicle vehicle) {
+        int containerType = container.getType();
+        int vehicleType = vehicle.getType();
+
+        if (vehicleType == 1) {
+            // Ships can carry all container types
+            return true;
+        } else if (vehicleType == 2) {
+            // Basic trucks can carry dry storage, open top, and open side containers
+            return containerType >= 1 && containerType <= 3;
+        } else if (vehicleType == 3) {
+            // Reefer trucks can carry refrigerated containers
+            return containerType == 4;
+        } else if (vehicleType == 4) {
+            // Tanker trucks can carry liquid containers
+            return containerType == 5;
+        } else {
+            // Unknown vehicle type, not compatible with any containers
+            return false;
         }
     }
+
+    static boolean isDestinationPortAvailable(Port destinationPort, Vehicle selectedVehicle) {
+        int currentContainerNum = 0;
+        for (Container container : destinationPort.getContainers()) {
+            currentContainerNum++;
+        }
+        int remainingContainerSlot = destinationPort.getStoringCap() - currentContainerNum;
+        if (remainingContainerSlot == 0) {
+            return false;
+        }
+        if (selectedVehicle.getType() != 1) {
+            int currentVehicleNum = 0;
+            for (Vehicle vehicle : destinationPort.getVehicles()) {
+                currentVehicleNum++;
+            }
+            int remainingVehicleSlot = destinationPort.getLandingCap() - currentVehicleNum;
+            if (remainingVehicleSlot == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static double totalFuelConsumptionForTrip(List<Container> selectedContainers, Port startingPort, Port destinationPort, Vehicle selectedVehicle) {
+        double fuelConsumptionPerKm = 0;
+
+        // Initialize fuel consumption based on vehicle type
+        if (selectedVehicle.getType() == 1) {
+            // For ships (type 1), set an initial value
+            fuelConsumptionPerKm = 1.0; // You can adjust this value as needed
+        } else {
+            // For other vehicle types (types 2 to 4), set a different initial value
+            fuelConsumptionPerKm = 2.0; // You can adjust this value as needed
+        }
+
+        for (Container container : selectedContainers) {
+            System.out.println(container.getContainerTypeName());
+            switch (container.getType()) {
+                case 1:
+                    fuelConsumptionPerKm += 3.5;
+                    break;
+                case 2:
+                    fuelConsumptionPerKm += 2.8;
+                    break;
+                case 3:
+                    fuelConsumptionPerKm += 2.7;
+                    break;
+                case 4:
+                    fuelConsumptionPerKm += 4.5;
+                    break;
+                case 5:
+                    fuelConsumptionPerKm += 4.8;
+                    break;
+            }
+        }
+
+        double distanceBetweenPort = Math.sqrt(Math.pow((destinationPort.getLatitude() - startingPort.getLatitude()), 2) + Math.pow((destinationPort.getLongitude() - startingPort.getLongitude()), 2));
+        return distanceBetweenPort * fuelConsumptionPerKm;
+    }
+
     public static void main(String[] args) {
         createTrip();
     }
