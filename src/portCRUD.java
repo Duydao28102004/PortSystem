6ac4e9e62fd89d799c03f5dbde2e61118757ab4a
Duydao28102004 <duybaodao28102005@gmail.com>
2023-09-20 01:17:22 +0700

    Fix update port bug

--- src/portCRUD.java
+++ src/portCRUD.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class portCRUD {
+public class PortCRUD {
     static void createPort() {
         ArrayList<Port> ports = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
@@ -102,20 +102,19 @@ public class portCRUD {
             return;
         }
         int portID = tempPortID - 1;
-        enterUpdatePortInfo(ports, portID);
-        scanner.close();
+        enterUpdatePortInfo(ports, portID, scanner);
         // save the port back to file again
         writeBackToFile(ports, false);
     }
     static void updateSpecificPort(int PortID) {
+        Scanner scanner = new Scanner(System.in);
         int portID = PortID - 1;
         List<Port> ports = readPorts();
-        enterUpdatePortInfo(ports, portID);
+        enterUpdatePortInfo(ports, portID, scanner);
         writeBackToFile(ports, false);
     }
-    static void enterUpdatePortInfo(List<Port> ports, int portID) {
+    static void enterUpdatePortInfo(List<Port> ports, int portID, Scanner scanner) {
         // ask user for information they want to update
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter new port name (Enter skip to skip): ");
         String temp1 = scanner.next();
         if (!temp1.equals("skip")) {
@@ -143,7 +142,7 @@ public class portCRUD {
         }
     }
     static void deletePort() {
-        List<Port> ports = containerCRUD.readContainer();
+        List<Port> ports = ContainerCRUD.readContainer();
         Scanner scanner = new Scanner(System.in);
         // print out port list and ask user for port ID they want to delete
         printPorts();
@@ -169,7 +168,7 @@ public class portCRUD {
             ports.get(i).setP_number(i + 1);
         }
         writeBackToFile(ports, false);
-        containerCRUD.writeBackToFileContainer(ports);
+        ContainerCRUD.writeBackToFileContainer(ports);
     }
     static void writeBackToFile(List<Port> ports, Boolean append) {
         try {
